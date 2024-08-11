3759893b4309ffea9d38f7f1cd5031cf0885a1a6
Sadique Hussain <106325596+sadique773@users.noreply.github.com>
2024-08-11 22:27:53 +0530

    To Do

---
+++ 03_Javascript/Part-4/app.js
@@ -0,0 +1,10 @@
+document.querySelector("#Name").innerText="Sadique Hussain"
+document.querySelector(".Info").innerText = "sjbdjsbds"
+document.querySelector("h3").innerText = "knkldnfdsf"
+
+
+let share = document.querySelector(".share")
+
+share.addEventListener("click",()=>{
+    alert("Photo shared")
+})
