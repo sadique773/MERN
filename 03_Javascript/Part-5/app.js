ddc98bb730ed47c2f59d9c2f1952a18e740c67f2
Sadique Hussain <106325596+sadique773@users.noreply.github.com>
2024-08-12 08:11:36 +0530

    Simon game

--- 03_Javascript/Part-5/app.js
+++ 03_Javascript/Part-5/app.js
@@ -1,10 +1,31 @@
 let btn = document.querySelector("button");
-let ul = document.querySelector("ul");
+let ol = document.querySelector("ol");
 let inp = document.querySelector("input");
+
+
+
 
 btn.addEventListener("click",function() {
    let item = document.createElement("li");
    item.innerText = inp.value;
-   ul.appendChild(item);
+
+   let delBtn = document.createElement("button");
+   delBtn.innerText ="Delete";
+   delBtn.classList.add("delete")
+
+
+   item.appendChild(delBtn);
+   ol.appendChild(item);
    inp.value="";
+});
+
+ol.addEventListener("click",function(event){
+   if(event.target.nodeName == "BUTTON"){
+    let listitem = event.target.parentElement;
+    listitem.remove();
+    
+   }
 })
+
+
+
