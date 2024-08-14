4e88bcd309f2d31efda6ef1eff045338a05e6923
Sadique Hussain <106325596+sadique773@users.noreply.github.com>
2024-08-14 16:32:58 +0530

    React-Start

---
+++ 04_React/Part-1/basic-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Title from "./Title.jsx"
+
+function App() {
+  return <>
+      <button>Click me</button>
+      <Title/>
+      <Title/>
+  </>
+
+}
+
+export default App
+ 
---
+++ 04_React/Part-1/basic-react-app/src/Title.jsx
@@ -0,0 +1,4 @@
+function Title(){
+    return <h1>I am Titile component</h1>
+}
+export default Title;
