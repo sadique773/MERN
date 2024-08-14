d20f013dd443516d2dc08c9fed12883af94e60ba
Sadique Hussain <106325596+sadique773@users.noreply.github.com>
2024-08-14 17:58:05 +0530

    Part-1

--- 04_React/Part-1/basic-react-app/src/App.jsx
+++ 04_React/Part-1/basic-react-app/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
 import Title from "./Title.jsx"
+import Product from "./Product.jsx"
+import ProductTab from './ProductTab.jsx'
+
 
 function App() {
-  return <>
-      <button>Click me</button>
-      <Title/>
-      <Title/>
-  </>
+  let name = "Sadique Huzefa"
+  return (
+    <>
+      <p>Hey! I am {name}</p>
+      <Title/>       
+      <ProductTab/>
+    </>
+  );
+
 
 }
 
---
+++ 04_React/Part-1/basic-react-app/src/Product.jsx
@@ -0,0 +1,10 @@
+import "./Product.css"
+function Product(){
+    return(
+        <div className="Product">
+            <h3>This is my Product Title</h3>
+            <h4>Product description</h4>
+        </div>
+    )
+}
+export default Product;
